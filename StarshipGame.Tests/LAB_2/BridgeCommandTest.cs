c1a0d5ba5ace78b97fb2bcfa852691e6c7dd4d4d
Vadim-Eryomin <vadim.eremin.2005@mail.ru>
2024-11-05 21:49:40 +0600

    feat: BridgeCommand and tests added

---
+++ StarshipGame.Tests/LAB_2/BridgeCommandTest.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+
+namespace StarshipGame.Test;
+
+public class BridgeCommandTest
+{
+    /***
+     * tests:
+     * 1) good work
+     * 2) command is empty and exception
+     */
+
+    [Fact]
+    public void BridgeWorking()
+    {
+        Mock<ICommand> innerCommand = new Mock<ICommand>();
+        innerCommand.Setup(e => e.Execute());
+        BridgeCommand bridge = new BridgeCommand();
+        bridge.Inject(innerCommand.Object);
+
+        bridge.Execute();
+
+        innerCommand.Verify(mock => mock.Execute(), Times.Once());
+    }
+
+    [Fact]
+    public void BridgeCalledWithNoCommand()
+    {
+        BridgeCommand bridge = new BridgeCommand();
+        Assert.Throws<NullReferenceException>(() => bridge.Execute());
+    }
+}
---
+++ StarshipGame/LAB_2/BridgeCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace StarshipGame;
+
+public class BridgeCommand : ICommand, IInjectable
+{
+    private ICommand _cmd;
+
+    public void Execute()
+    {
+        _cmd.Execute();
+    }
+
+    public void Inject(ICommand cmd)
+    {
+        _cmd = cmd;
+    }
+}

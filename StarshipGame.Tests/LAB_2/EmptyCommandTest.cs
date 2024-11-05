ecd61f5fc391f2b5c07476dfe050f3326e0665bf
Vadim-Eryomin <49606493+Vadim-Eryomin@users.noreply.github.com>
2024-11-05 21:32:57 +0600

    Empty (#4)
    * feat: EmptyCommand
    
    * style: format code

---
+++ StarshipGame.Tests/LAB_2/EmptyCommandTest.cs
@@ -0,0 +1,11 @@
+﻿namespace StarshipGame.Test;
+
+public class EmptyCommandTest
+{
+    [Fact]
+    public void TestEmptyCommand()
+    {
+        EmptyCommand cmd = new EmptyCommand();
+        cmd.Execute();
+    }
+}

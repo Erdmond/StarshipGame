16b476125f3d5e4c7b68d9376cbe6816690b0d61
Vadim-Eryomin <vadim.eremin.2005@mail.ru>
2024-11-06 13:47:15 +0600

    feat: add BridgeCommand and tests

---
+++ StarshipGame/BridgeCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace StarshipGame;
+
+public class BridgeCommand: ICommand, IInjectable
+{
+    private ICommand cmd;
+
+    public void Execute()
+    {
+        cmd.Execute();
+    }
+
+    public void Inject(ICommand command)
+    {
+        cmd = command;
+    }
+}

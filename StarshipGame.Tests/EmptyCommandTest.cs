51ea12020861bff7199606f8859309487abb392c
Vadim-Eryomin <vadim.eremin.2005@mail.ru>
2024-11-06 13:34:37 +0600

    feat: add Empty command and test for it

---
+++ StarshipGame.Tests/EmptyCommandTest.cs
@@ -0,0 +1,11 @@
+﻿namespace StarshipGame.Tests;
+
+public class EmptyCommandTest
+{
+    [Fact]
+    public void WorkProperly()
+    {
+        EmptyCommand cmd = new EmptyCommand();
+        cmd.Execute();
+    }
+}
---
+++ StarshipGame/EmptyCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace StarshipGame;
+
+public class EmptyCommand : ICommand
+{
+    public void Execute()
+    {
+        // nothing to do...
+    }
+}

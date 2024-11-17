62c6ff5be279e162c130d0b353ba663fb7ecac5e
NesterovaAnna <nesterova.anna.alex@yandex.ru>
2024-11-17 21:13:28 +0600

    MoveCommand

---
+++ StarshipGame/Vector.cs
@@ -0,0 +1,31 @@
+namespace StarshipGame;
+
+public class Vector
+{
+    private int[] data;
+
+    public Vector(int[] data)
+    {
+        this.data = data;
+    }
+
+    public static Vector operator +(Vector a, Vector b)
+    {
+        var res = new Vector(a.data.Select((e, i) => e + b.data[i]).ToArray());
+        return res;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Vector other)
+        {
+            return data.SequenceEqual(other.data);
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return data.Aggregate(17, (current, item) => current * 31 + item);
+    }
+}

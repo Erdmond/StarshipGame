5f6bfa151d4ca45fcfc28021e90a6ae806894033
Vadim-Eryomin <vadim.eremin.2005@mail.ru>
2024-12-14 12:17:48 +0600

    feat: add MacroCommand: tests and realisation feat: add MacroCommand Registration strategy

---
+++ StarshipGame/MacroCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+namespace StarshipGame;
+
+
+public class MacroCommand: Hwdtech.ICommand
+{
+    private Hwdtech.ICommand[] cmds;
+    public MacroCommand(Hwdtech.ICommand[] cmds)
+    {
+        this.cmds = cmds;
+    }
+
+    public void Execute()
+    {
+        cmds.ToList().ForEach(c => c.Execute());
+    }
+}
---
+++ StarshipGame/RegisterIoCDependencyMacroCommand.cs
@@ -0,0 +1,14 @@
+﻿using Hwdtech;
+
+namespace StarshipGame;
+
+public class RegisterIoCDependencyMacroCommand: Hwdtech.ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Commands.GetICommandsFromArgs",
+            (object[] args) => args.ToList().Select(c => (Hwdtech.ICommand)c).ToArray()).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Commands.Macro", 
+            (object[] args) => new MacroCommand(IoC.Resolve<Hwdtech.ICommand[]>("Commands.GetICommandsFromArgs", args))).Execute();
+    }
+}

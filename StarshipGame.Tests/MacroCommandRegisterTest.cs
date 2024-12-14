69c02bbbf19cb4973a6efb7f7cef70087132e11c
Vadim-Eryomin <vadim.eremin.2005@mail.ru>
2024-12-14 12:17:48 +0600

    feat: add MacroCommand: tests and realisation feat: add MacroCommand Registration strategy

---
+++ StarshipGame.Tests/MacroCommandRegisterTest.cs
@@ -0,0 +1,22 @@
+﻿using Hwdtech;
+using Hwdtech.Ioc;
+
+namespace StarshipGame.Tests;
+
+public class MacroCommandRegisterTest
+{
+    public MacroCommandRegisterTest()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set",
+            IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+    }
+    
+
+    [Fact]
+    public void RegisterIoCDependencyMacroCommandExecuteAndDependencyResolved()
+    {
+        new RegisterIoCDependencyMacroCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Commands.Macro");
+    }
+}

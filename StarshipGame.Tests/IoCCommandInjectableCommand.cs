2fbc51dc21d65656d07101b208ab4a8a7de3b68e
Vadim-Eryomin <vadim.eremin.2005@mail.ru>
2024-12-16 08:24:23 +0600

    feat: add RegisterCommandInjectableCommand - Command Injectable Command now available from IoC

---
+++ StarshipGame.Tests/IoCCommandInjectableCommand.cs
@@ -0,0 +1,24 @@
+﻿using Hwdtech;
+using Hwdtech.Ioc;
+
+namespace StarshipGame.Tests;
+
+public class IoCCommandInjectableCommand
+{
+    public IoCCommandInjectableCommand()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set",
+            IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+    }
+
+    [Fact]
+    public void GenericVariantsOfCommandInjectableDoesNotExcept()
+    {
+        new RegisterCommandInjectableCommand().Execute();
+        
+        IoC.Resolve<Hwdtech.ICommand>("Commands.CommandInjectable");
+        IoC.Resolve<ICommandInjectable>("Commands.CommandInjectable");
+        IoC.Resolve<CommandInjectableCommand>("Commands.CommandInjectable");
+    }
+}

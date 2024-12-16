7f8e06f58c0828022abb7b8202fa9d145a739bd3
Vadim-Eryomin <vadim.eremin.2005@mail.ru>
2024-12-16 08:59:46 +0600

    feat: register Macro.Move and Macro.Rotate and tests for them

---
+++ StarshipGame/RegisterIoCDependencyMacroMoveRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Hwdtech;
+
+namespace StarshipGame;
+
+public class RegisterIoCDependencyMacroMoveRotate : Hwdtech.ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Macro.Move",
+            (object[] args) =>
+                IoC.Resolve<Hwdtech.ICommand>("Commands.Macro", IoC.Resolve<string[]>("Specs.Move").ToList()
+                    .Select(commandName => IoC.Resolve<Hwdtech.ICommand>(commandName, args)).ToArray())
+        ).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Macro.Rotate",
+            (object[] args) =>
+                IoC.Resolve<Hwdtech.ICommand>("Commands.Macro", IoC.Resolve<string[]>("Specs.Rotate").ToList()
+                    .Select(commandName => IoC.Resolve<Hwdtech.ICommand>(commandName, args)).ToArray())
+        ).Execute();
+    }
+}

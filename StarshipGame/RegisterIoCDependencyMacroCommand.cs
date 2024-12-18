d5e575ab3d196cead4d623f2df0242246672cd4c
Vadim-Eryomin <vadim.eremin.2005@mail.ru>
2024-12-18 13:25:32 +0600

    fix: change type of registered dependency to correspond last commit

--- StarshipGame/RegisterIoCDependencyMacroCommand.cs
+++ StarshipGame/RegisterIoCDependencyMacroCommand.cs
@@ -7,7 +7,7 @@ public class RegisterIoCDependencyMacroCommand : Hwdtech.ICommand
     public void Execute()
     {
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Commands.GetICommandsFromArgs",
-            (object[] args) => args.ToList().Select(c => (Hwdtech.ICommand)c).ToArray()).Execute();
+            (object[] args) => args.ToList().Select(c => (Hwdtech.ICommand)c).ToList()).Execute();
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Commands.Macro",
                 (object[] args) =>
                     new MacroCommand(IoC.Resolve<List<Hwdtech.ICommand>>("Commands.GetICommandsFromArgs", args)))

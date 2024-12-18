6007833ef623c28b51f68b75f88132f9d38fd84d
EgorDanilovv <e325361@gmail.com>
2024-12-18 14:10:49 +0600

    IoC-Action.Stop

---
+++ StarshipGame.Tests/RegisterIoCDependencyActionsStopTest.cs
@@ -0,0 +1,27 @@
+namespace StarshipGame.Tests;
+using Moq;
+using Hwdtech;
+using Hwdtech.Ioc;
+
+public class RegisterIoCDependencyActionsStopTests
+{
+    public RegisterIoCDependencyActionsStopTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+    }
+
+    [Fact]
+    public void Execute_ShouldResolveDependency_WhenInvoked()
+    {
+        var registerCommand = new RegisterIoCDependencyActionsStop();
+        registerCommand.Execute();
+
+        var mockDictionary = new Mock<IDictionary<string, object>>();
+
+        var actionStop = IoC.Resolve<StarshipGame.ICommand>("Actions.Stop", mockDictionary.Object);
+
+        Assert.NotNull(actionStop);
+        Assert.IsType<EmptyCommand>(actionStop);
+    }
+}

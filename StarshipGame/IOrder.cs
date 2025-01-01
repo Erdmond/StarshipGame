2dbc90bb67635c84815a221f01736b5f9d18d008
Izgorodin Ilya FIT-232 <kekizgorodyn@mail.ru>
2025-01-02 01:22:27 +0500

    chore: rename & relocate files

--- StarshipGame/IOrder.cs
+++
@@ -1,7 +0,0 @@
-namespace StarshipGame;
-
-public interface IOrder
-{
-    IObject Target { get; }
-    Dictionary<string, object> InitialValues { get; }
-}
--- StarshipGame/IQueue.cs
+++
@@ -1,8 +0,0 @@
-namespace StarshipGame;
-using Hwdtech;
-
-public interface IQueue
-{
-    void Put(Hwdtech.ICommand cmd);
-    Hwdtech.ICommand Take();
-}

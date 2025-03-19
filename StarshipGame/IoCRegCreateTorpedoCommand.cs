dfb32695afbc415ce35235f712411daa4c35bfe9
NesterovaAnna <nesterova.anna.alex@yandex.ru>
2025-03-19 13:21:12 +0600

    chore: delete all

--- StarshipGame/IoCRegCreateTorpedoCommand.cs
+++
@@ -1,8 +0,0 @@
-namespace StarshipGame;
-public class RegisterIoCDependencyTorpedoCommand : ICommand
-{
-    public void Execute()
-    {
-        IoC.Resolve<ICommand>("IoC.Register", "Commands.Create.Torpedo", (object[] args) => new Torpedo(((IMovable)args[0]).Position, ((IMovable)args[0]).Velocity)).Execute();
-    }
-}

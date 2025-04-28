08f5503f0fc7ff378a12aa671b5bd649c4ab38c7
vadim <vadim.eremin.2005@mail.ru>
2025-04-28 16:05:11 +0600

    ауф: поля и адаптеры

---
+++ StarshipGame/Field.cs
@@ -0,0 +1,17 @@
+﻿namespace StarshipGame;
+
+public class Field(
+    string type,
+    string name,
+    bool isDefaultGetter,
+    bool isDefaultSetter,
+    string? customGetter,
+    string? customSetter)
+{
+    public string Type => type;
+    public string Name => name;
+    public bool IsDefaultGetter => isDefaultGetter;
+    public bool IsDefaultSetter => isDefaultSetter;
+    public string? CustomGetter => customGetter;
+    public string? CustomSetter => customSetter;
+}
---
+++ StarshipGame/IoCRegMakeAdapterLineCommand.cs
@@ -0,0 +1,56 @@
+﻿using Scriban;
+
+namespace StarshipGame;
+
+public class IoCRegMakeAdapterCommand : ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<ICommand>("IoC.Register", "Adapters.MakeField", (object[] args) =>
+                new Field((string)args[0], (string)args[1], (bool)args[2],
+                    (bool)args[3], (string)args[4], (string)args[5]))
+            .Execute();
+
+        // interface, fields[]
+        IoC.Resolve<ICommand>("IoC.Register", "Adapters.CreateAdapter", (object[] args) =>
+        {
+            string interfaceName = (string)args[0];
+            Field[] fields = (Field[])args[1];
+
+            string[] fieldValues = fields
+                .Select(field =>
+                {
+                    string getter = field.IsDefaultGetter
+                        ? Getter(field.Type, field.Name)
+                        : CustomGetter(field.CustomGetter);
+                    string setter = field.IsDefaultSetter
+                        ? Setter(field.Type, field.Name)
+                        : CustomSetter(field.CustomSetter);
+
+                    return Template.ParseLiquid("public {{type}} {{name}} { {{getter}} {{setter}} }")
+                        .Render(new { Type = field.Type, Name = field.Name, Getter = getter, Setter = setter });
+                }).ToArray();
+            
+            string fieldsInString = string.Join(" ", fieldValues);
+
+            return Template.ParseLiquid("class {{name}}Adapter: {{interfaceName}} { {{fields}} }")
+                .Render(new {Name = interfaceName.Substring(1), IntefaceName = interfaceName, Fields = fieldsInString});
+        }).Execute();
+    }
+
+    public string Getter(string type, string name)
+        => Template.ParseLiquid("get => ({{type}}) startObject[\"{{name}}\"];")
+            .Render(new { Type = type, Name = name });
+
+    public string Setter(string type, string name)
+        => Template.ParseLiquid("set => startObject[\"{{name}}\"] = ({{type}})value;")
+            .Render(new { Type = type, Name = name });
+
+    public string CustomGetter(string customGetter)
+        => Template.ParseLiquid("get => ({{type}}) {{customGetter}}(startObject);")
+            .Render(new { CustomGetter = customGetter });
+
+    public string CustomSetter(string customSetter)
+        => Template.ParseLiquid("set => {{customSetter}}(startObject, value);")
+            .Render(new { CustomSetter = customSetter });
+}

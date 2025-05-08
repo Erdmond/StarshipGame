f77ff4a7c719f8a12399a412c427dae8a74eb734
Izgorodin Ilya FIT-232 <kekizgorodyn@mail.ru>
2025-05-08 17:05:02 +0600

    test

--- StarshipGame/IoCRegCollectInterfaces.cs
+++ StarshipGame/IoCRegCollectInterfaces.cs
@@ -1,11 +1,24 @@
 ﻿namespace StarshipGame;
 
-public class IoCRegCollectInterfaces: ICommand
+using Hwdtech;
+using System.Reflection;
+
+public class IoCRegCollectInterfaces : ICommand
 {
-    
-    
     public void Execute()
     {
-        
+        IoC.Resolve<ICommand>(
+            "IoC.Register",
+            "Commands.CollectInterfaces",
+            (object[] args) =>
+            {
+                var commandTypes = args.FirstOrDefault() as IEnumerable<Type> ?? Enumerable.Empty<Type>();
+                
+                return commandTypes
+                    .Select(type => type.GetConstructors()[0].GetParameters()[0].ParameterType)
+                    .Distinct()
+                    .ToList();
+            }
+        ).Execute();
     }
 }

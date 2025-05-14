694a47a8d0f64b22b018b9d51e6dbeebb8348c3c
EgorDanilovv <e325361@gmail.com>
2025-05-14 13:26:44 +0600

    IoCRegCollect_and_IoCRegFind

---
+++ StarshipGame.Tests/IoCRegFindCommandsTest.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+using Hwdtech.Ioc;
+using Moq;
+
+namespace StarshipGame.Tests;
+
+public class IoCRegFindCommandsTest
+{
+    interface ITest
+    {
+    }
+
+    class MyTestCommand : ICommand
+    {
+        public MyTestCommand(ITest test)
+        {
+        }
+
+        public void Execute()
+        {
+            // nothing to do...
+        }
+    }
+
+    public IoCRegFindCommandsTest()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<ICommand>("Scopes.Current.Set",
+            IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+
+        new RegisterIoCDependencyFindCommands().Execute();
+    }
+
+    [Fact]
+    public void EmptyOnNoAssembly()
+    {
+        var types = IoC.Resolve<List<Type>>("Commands.FindCommands");
+        Assert.Empty(types);
+    }
+
+    [Fact]
+    public void ReturnTypesOfAssembly()
+    {
+        Mock<Assembly> assemblyMock = new();
+        assemblyMock.Setup(a => a.GetTypes()).Returns([typeof(MyTestCommand)]);
+
+        var types = IoC.Resolve<List<Type>>("Commands.FindCommands", assemblyMock.Object);
+
+        Assert.NotEmpty(types);
+        Assert.Equal(types, [typeof(MyTestCommand)]);
+    }
+
+    [Fact]
+    public void NullThrowsArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => IoC.Resolve<List<Type>>("Commands.FindCommands", null));
+    }
+}
---
+++ StarshipGame/IoCRegFindCommands.cs
@@ -0,0 +1,22 @@
+namespace StarshipGame;
+
+using Hwdtech;
+using System.Reflection;
+
+public class RegisterIoCDependencyFindCommands : Hwdtech.ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            "Commands.FindCommands",
+            (object[] args) =>
+                args.Select(a => (Assembly)a)
+                    .SelectMany(assembly => assembly.GetTypes())
+                    .Where(type => typeof(ICommand).IsAssignableFrom(type) && !type.IsAbstract && !type.IsInterface)
+                    .Where(t => t.GetConstructors(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                        .All(cn => cn.GetParameters().Length == 1 && cn.GetParameters()[0].ParameterType.IsInterface))
+                    .ToList()
+        ).Execute();
+    }
+}

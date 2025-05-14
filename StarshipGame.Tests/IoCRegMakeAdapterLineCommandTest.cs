4de4dc37b3b9296b9df06f3c9c6ed9bb304c1e00
Erdmond <144764219+Erdmond@users.noreply.github.com>
2025-05-14 13:35:12 +0600

    feat: code strings generation and types-along (#101)
    Co-authored-by: vadim <vadim.eremin.2005@mail.ru>

---
+++ StarshipGame.Tests/IoCRegMakeAdapterLineCommandTest.cs
@@ -0,0 +1,102 @@
+﻿using Hwdtech.Ioc;
+
+namespace StarshipGame.Tests;
+
+public class IoCRegMakeAdapterLineCommandTest
+{
+
+    interface ITest
+    {
+        public int TestInt { get; set; }
+        public string TestString { get; set; }
+    }
+
+    public class MyClass
+    {
+        public static string GetTestString(IDictionary<object, object> startObject)
+        {
+            return "hello";
+        }
+
+        public static void SetTestString(IDictionary<object, object> startObject, string val)
+        {
+            // something did...
+        }
+    }
+
+    public IoCRegMakeAdapterLineCommandTest()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<ICommand>("Scopes.Current.Set",
+            IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+
+        new IoCRegMakeAdapterCommand().Execute();
+    }
+
+    [Fact]
+    public void MakeFieldCommandCreatesFieldSuccessfully()
+    {
+        var field = IoC.Resolve<Field>(
+            "Adapters.MakeField",
+            "int", "SomeField", true, true, null, null, true
+        );
+
+        Assert.Equal("int", field.Type);
+        Assert.Equal("SomeField", field.Name);
+        Assert.True(field.IsDefaultGetter);
+        Assert.True(field.IsDefaultSetter);
+        Assert.Null(field.CustomGetter);
+        Assert.Null(field.CustomSetter);
+        Assert.True(field.NeedSetter);
+    }
+
+    [Fact]
+    public void MakeAdapterLineProperlyWithAllFieldsFactoryAndIocStrategy()
+    {
+        string adapter = IoC.Resolve<string>("Adapters.CreateAdapter", "ITest", new[]
+        {
+            new Field("int", "TestInt", true, true, null, null, true),
+            new Field("string", "TestString", false, false, "MyClass.GetTestString", "MyClass.SetTestString", true)
+        });
+
+        Assert.Equal(@"class TestAdapter: ITest { 
+                IDictionary<object, object> startObject; 
+                public TestAdapter(IDictionary<object, object> _startObject) 
+                { startObject = _startObject; } 
+
+                public int TestInt { get => (int) startObject[""TestInt""]; set => startObject[""TestInt""] = (int)value; } public string TestString { get => MyClass.GetTestString(startObject); set => MyClass.SetTestString(startObject, value); } 
+                }  
+                class TestFactory: IFactory, ICommand
+                { 
+                public object Adapt(IDictionary<object, object> startObject) 
+                { return new TestAdapter(startObject); } 
+
+                public void Execute() 
+                { IoC.Resolve<ICommand>(""IoC.Register"", ""Adapters.Test"", (object[] args) => this.Adapt((IDictionary<object, object>) args[0])).Execute(); } 
+                }", adapter);
+    }
+
+    [Fact]
+    public void UntitledAdapterThrowsException()
+    {
+        Assert.Throws<ArgumentException>(() => IoC.Resolve<string>("Adapters.CreateAdapter", "", new Field[] { }));
+    }
+
+    [Fact]
+    public void OnlyIClassThrowsException()
+    {
+        Assert.Throws<ArgumentException>(() => IoC.Resolve<string>("Adapters.CreateAdapter", "I", new Field[] { }));
+    }
+
+    [Fact]
+    public void WithoutFieldsThrowsException()
+    {
+        Assert.Throws<IndexOutOfRangeException>(() => IoC.Resolve<string>("Adapters.CreateAdapter", "IInterface"));
+    }
+
+    [Fact]
+    public void NullFieldsThrowsException()
+    {
+        Assert.Throws<ArgumentNullException>(() => IoC.Resolve<string>("Adapters.CreateAdapter", "IInterface", null));
+    }
+}
---
+++ StarshipGame/IoCRegCommandRegisterStringMake.cs
@@ -0,0 +1,37 @@
+﻿using Scriban;
+
+namespace StarshipGame;
+
+public class IoCRegCommandRegisterStringMake : ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<ICommand>("IoC.Register", "Adapters.Command.MakeLine", (object[] args) =>
+        {
+            if (args[0] is not string commandName || string.IsNullOrWhiteSpace(commandName))
+                throw new ArgumentException("Invalid Command Name");
+
+            if (args[1] is not string className || string.IsNullOrWhiteSpace(className))
+                throw new ArgumentException("Invalid Class Name");
+
+            var template = @"
+using System;
+using System.Collections.Generic;
+
+public class {{command}}Factory : ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<ICommand>(""IoC.Register"", ""Commands.{{command}}"",
+            (object[] args) => new {{command}}(
+                IoC.Resolve<I{{cls}}>(""Adapters.{{cls}}"", args[0])
+            )
+        ).Execute();
+    }
+}";
+
+            return Template.ParseLiquid(template)
+                .Render(new { command = commandName, cls = className });
+        }).Execute();
+    }
+}

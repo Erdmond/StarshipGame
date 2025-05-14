c572d928f90e8d63acfa9afdbe8f267b0a3b2c7f
Erdmond <144764219+Erdmond@users.noreply.github.com>
2025-05-14 13:41:42 +0600

    feat: activate adapters (#102)
    Co-authored-by: KusainovaSaule <145184823+KusainovaSaule@users.noreply.github.com>

---
+++ StarshipGame/IoCRegFactoryReg.cs
@@ -0,0 +1,58 @@
+using System.Reflection;
+
+namespace StarshipGame;
+
+public class RegisterIoCDependencyFactoryReg : ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            "FactoriesRegister",
+            (object[] args) =>
+            {
+                List<Type> interfaces = (List<Type>)args[0];
+
+                var adapterStrings = interfaces.Select((Type i, int index) =>
+                {
+                    var fields = i.GetProperties().Select((PropertyInfo pi, int piIndex) =>
+                    {
+                        var defaultGetter = IoC.Resolve<bool>("Adapter.IsComputed", pi.Name, true);
+                        var defaultSetter = IoC.Resolve<bool>("Adapter.IsComputed", pi.Name, false);
+
+                        return new Field(
+                            pi.PropertyType.Name,
+                            pi.Name,
+                            defaultGetter,
+                            defaultSetter,
+                            defaultGetter ? null : IoC.Resolve<string>("Adapter.ComputeMethod", pi.Name, true),
+                            defaultSetter ? null : IoC.Resolve<string>("Adapter.ComputeMethod", pi.Name, false),
+                            pi.CanWrite
+                        );
+                    }).ToArray();
+
+                    return IoC.Resolve<string>("Adapters.CreateAdapter", i.Name, fields);
+                }).ToArray();
+
+                adapterStrings
+                    .Select<string, object>((string adapterString, int index) =>
+                    {
+                        IoC.Resolve<Hwdtech.ICommand>("Commands.ActivateCommand", adapterString).Execute();
+                        return (object)null;
+                    })
+                    .ToArray();
+
+                interfaces
+                    .Select<Type, object>((Type interfaceType, int index) =>
+                    {
+                        var factoryCommand = IoC.Resolve<Hwdtech.ICommand>("Factories.Create", interfaceType);
+                        factoryCommand.Execute();
+                        return (object)null;
+                    })
+                    .ToArray();
+
+                return (object)null;
+            }
+        ).Execute();
+    }
+}
